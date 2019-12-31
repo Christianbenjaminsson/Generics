85f489f62aabad4920cd2289388f8f0f19934b86
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-12-31 10:35:20 +0100

    Added Team class

--- src/com/example/generics/Main.java
+++ src/com/example/generics/Main.java
@@ -8,12 +8,41 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        Team adelaideCrows = new Team("Adelaide Crows");
+        Team<FotballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(beckham);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
 
         System.out.println(adelaideCrows.numPlayers());
 
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FotballPlayer> melbourne = new Team<>("Melbourne");
+        FotballPlayer banks = new FotballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FotballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FotballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle,2,1);
+//        adelaideCrows.matchResult(baseballTeam, 1,1);
+
+        System.out.println("Ranking");
+        System.out.println(adelaideCrows.getName() + ": " + adelaideCrows.ranking());
+        System.out.println(melbourne.getName() + ": " + melbourne.ranking());
+        System.out.println(fremantle.getName() + ": " + fremantle.ranking());
+        System.out.println(hawthorn.getName() + ": " + hawthorn.ranking());
+
+        System.out.println(adelaideCrows.compareTo(melbourne));
+        System.out.println(adelaideCrows.compareTo(hawthorn));
+        System.out.println(hawthorn.compareTo(adelaideCrows));
+        System.out.println(melbourne.compareTo(fremantle));
     }
 }
